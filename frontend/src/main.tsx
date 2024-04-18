725536db2892fffcbec7e69277cf5a86ddba7c83
Arttu-Nurminoro <arttunurminoro@gmail.com>
2024-04-18 15:15:55 +0300

    router and navbar things

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+   RouterProvider,
+  } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
+import Home from './pages/home.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        children: [
+          {index: true, element: <Home/>},
+        ],
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+    return(
+        <>
+            <div>
+                <h1>Home pagings</h1>
+            </div>
+        </>
+    )
+}
