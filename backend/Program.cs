83fba9deb5a8863a0029344939a9cb9e38927f08
eannu <132340026+eannu@users.noreply.github.com>
2024-04-24 14:17:20 +0300

    db stuff works perfectly whoa

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,22 @@
+using System.Reflection.Metadata.Ecma335;
+using Npgsql;
+
+var connectionString = "host=localhost;port=5432;database=taskmanager;username=postgres;password=password;sslmode=prefer";
+
+await using var conn = new NpgsqlConnection(connectionString);
+await conn.OpenAsync();
+
+await SqiqqeliQuery.InsertStatus("sdkmf", "sdfkdfmskfsdkmsdfmksdf sdfm mksfd", conn);
+await SqiqqeliQuery.InsertTag("bennys", "tomato, conn", conn);
+await SqiqqeliQuery.InsertActivityType("benny", conn);
+await SqiqqeliQuery.InsertActivity("benny's pizza", "kebab", "pizza.keb.ab", conn);
+await SqiqqeliQuery.InsertTask("dfkmawefawef", "aowiefjaweofiawefojiawefioawef awef iaw fwe", conn);
+
+SqiqqeliQuery.SelectTask(conn);
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 
