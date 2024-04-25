d10c6cf88ad2fe461de3a68d40a3d74b7e9ca0c7
Miro <miro.airas@outlook.com>
2024-04-25 18:01:37 +0300

    tyylittelyjä

--- frontend/src/components/box.tsx
+++ frontend/src/components/box.tsx
@@ -3,9 +3,9 @@ import "./component.css";
 
 export default function Box({ children }) {
     return (
-        <div className="box min-h-fit h-3/4 w-full p-4 border-4 bg-white ml-5">
+        <div className="box min-h-fit h-3/4 w-full p-4 border-4 bg-white ml-5 mb-1 ">
             {children}
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full -ml-3 mt-2">
+            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full -ml-1 mt-2">
             Complete
             </button>
         </div>
